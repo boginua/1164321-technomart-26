63943256a46ede4e9c4fb9f826b5d8aa8ac4b0a2
boginua <boginua@rambler.ru>
2019-10-08 17:22:40 +0300

    Защита личного проекта «Техномарт» 2

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ if (modalFindUs) {
 	var modalText = modalFindUs.querySelector('.find-us-item-tarea');
 
 
-	modalFindUs.addEventListener('submit', function(evt){
+	modalFindUs.addEventListener('submit', function (evt) {
 
 		if (!modalName.value || !modalMail.value || !modalText.value) {
 			evt.preventDefault();
@@ -18,17 +18,17 @@ if (modalFindUs) {
 }
 var findUsButton = document.querySelector('.open-find-us');
 if (findUsButton) {
-	findUsButton.addEventListener('click', function(evt){
+	findUsButton.addEventListener('click', function (evt) {
 		evt.preventDefault();
 		modalFindUs.classList.add("modal-window-find-us-show");
 		modalName.focus();
 
-		modalFindUsClose.addEventListener('click', function(evtClose){
+		modalFindUsClose.addEventListener('click', function (evtClose) {
 			evtClose.preventDefault();
 			modalFindUs.classList.remove("modal-window-find-us-show");
 			modalFindUs.classList.remove('modal-window-find-us-error');
 		});
-		window.addEventListener('keydown', function(evtKey){
+		window.addEventListener('keydown', function (evtKey) {
 			if (evtKey.keyCode === 27) {
 				evtKey.preventDefault();
 				modalFindUs.classList.remove('modal-window-find-us-show');
@@ -39,10 +39,6 @@ if (findUsButton) {
 }
 
 
-
-
-
-
 var mapButton = document.querySelector('.contacts-map-link');
 
 var modalMap = document.querySelector('.modal-map');
@@ -51,16 +47,16 @@ if (modalMap) {
 }
 if (mapButton) {
 
-	mapButton.addEventListener('click', function(evt){
+	mapButton.addEventListener('click', function (evt) {
 
 		evt.preventDefault();
 		modalMap.classList.add('modal-map-show');
 
-		modalMapClose.addEventListener('click', function(evtClose){
+		modalMapClose.addEventListener('click', function (evtClose) {
 			evt.preventDefault();
 			modalMap.classList.remove('modal-map-show');
 		})
-		window.addEventListener('keydown', function(evtKey){
+		window.addEventListener('keydown', function (evtKey) {
 			if (evtKey.keyCode === 27) {
 				evtKey.preventDefault();
 				modalMap.classList.remove('modal-map-show');
@@ -69,28 +65,28 @@ if (mapButton) {
 	})
 }
 
-var addCartLinks = document.querySelectorAll('.button-add-to-cart'); 
+var addCartLinks = document.querySelectorAll('.button-add-to-cart');
 var cartWindow = document.querySelector('.modal-window');
 if (cartWindow) {
 	var cartWindowClose = cartWindow.querySelector('.modal-close');
 }
 if (addCartLinks) {
-	for (var i=0; i < addCartLinks.length; i++) {
-		addCartLinks[i].addEventListener('click', function(evt){
-		evt.preventDefault();
-		cartWindow.classList.add('modal-window-show');
-
-		cartWindowClose.addEventListener('click', function(evtClose){
+	for (var i = 0; i < addCartLinks.length; i++) {
+		addCartLinks[i].addEventListener('click', function (evt) {
 			evt.preventDefault();
-			cartWindow.classList.remove('modal-window-show');
-		})
+			cartWindow.classList.add('modal-window-show');
 
-		window.addEventListener('keydown', function(evtKey){
-			if (evtKey.keyCode === 27) {
-				evtKey.preventDefault();
+			cartWindowClose.addEventListener('click', function (evtClose) {
+				evt.preventDefault();
 				cartWindow.classList.remove('modal-window-show');
-			}
-		});
-	})
+			})
+
+			window.addEventListener('keydown', function (evtKey) {
+				if (evtKey.keyCode === 27) {
+					evtKey.preventDefault();
+					cartWindow.classList.remove('modal-window-show');
+				}
+			});
+		})
 	}
 }
